f4a0c4cb0fe4adb7bb060741cac879d5ec6f4d9d
Josh Weiner <joshuaweinernyc@gmail.com>
2019-01-31 23:19:52 -0500

    why was this not added

--- 00_canvas/clickdraw.js
+++ 00_canvas/clickdraw.js
@@ -6,9 +6,12 @@
 const c = document.getElementById("myCanvas"); //Canvas element
 // console.log(c); //Print statement for debugging
 const ctx = c.getContext("2d"); //Get context of canvas
+var clear_button = document.getElementById("clear");
 var draw_button = document.getElementById("draw"); //Element which toggles switch between dot & rectangle drawing mode
-var draw_mode = document.getElementById("mode"); //Element which displays current draw mode
-var dots_state = true; //State variable of whether or not user is drawing dots
+var msg = document.getElementById("msg"); //Element which displays current draw mode
+var dots_state = 0; //State variable of whether or not user is drawing dots
+//If divisible by 2, draw dots, otherwise draw rectangles
+var is_drawn = 0;
 
 //Adds event listener to the canvas
 //On mouse click within the canvas, will either create a dot or rectangle at those coordinates depending on the current state
@@ -19,38 +22,34 @@ c.addEventListener('click', function(e){
     var y = e.clientY - r.top; //Sets current mouse y-coordinate
 
     //If state is to draw dots, draw a dot at the coordinates
-    if (dots_state){
+    if (dots_state % 2 == 0){
+        ctx.fillStyle = "blue";
         ctx.beginPath();
         ctx.ellipse(x, y, 10, 10, 0, 0, 4 * Math.PI);
         ctx.fill();
     }
     //Else, draw a rectangle at the coordinates
     else {
-        ctx.fillRect(x - 20, y - 20, 40, 40);
+        ctx.fillStyle = "red";
+        ctx.fillRect(x, y, 40, 40);
     }
+    is_drawn += 1;
 })
 
 //Adds event listener to the toggle-drawing-mode button
-//Changes the state of the button, state variable, and current display of the drawing mode
+//Changes the current drawing mode
 draw_button.addEventListener('click', function(){
-    //If drawing dots, set toggle to on next click 'draw rectangles'
-    //Set current mode to display 'dots'
-    if (dots_state){
-        this.innerHTML = "draw dots";
-        mode.innerHTML = "Rectangles";
-    }
-    //Else, set toggle to on next click 'draw dots'
-    //Set current mode to display 'rectangles'
-    else {
-        this.innerHTML = "draw rectangles";
-        mode.innerHTML = "Dots";
-    }
-    //Changes the state with each click
-    dots_state = !dots_state;
+    dots_state += 1;
 });
 
-//Function, on click canvas will be cleared
-//State remains unchanged
-var clear_canvas = function() {
-    ctx.clearRect(0,0, c.width, c.height);
-}
+clear_button.addEventListener('click', function() {
+    if (is_drawn > 0) {
+        ctx.clearRect(0,0, c.width, c.height);
+        is_drawn = 0;
+        msg.innerHTML = "";
+    }
+    else {
+        event.preventDefault();
+        msg.innerHTML = "You must draw in the canvas to clear it!";
+    }
+})
